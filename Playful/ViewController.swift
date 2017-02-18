69409738a1a714af41f37556a74561c304697d31
David Brown <davidb@pixel.local>
2017-02-17 23:17:43 -0700

    Initial Commit

---
+++ Playful/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Playful
+//
+//  Created by David Brown on 2/17/17.
+//  Copyright © 2017 David Brown. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
