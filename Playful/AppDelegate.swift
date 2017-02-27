e2bb79eb41688c7dd2cfa8b995d04c0b7403bd57
David Brown <davidb@davidb.org>
2017-02-26 22:43:54 -0700

    Add the lesson json files to the program

--- Playful/AppDelegate.swift
+++ Playful/AppDelegate.swift
@@ -15,6 +15,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        let name = Bundle.main.path(forResource: "dict-canonical", ofType: "json")!
+        let url = URL(fileURLWithPath: name)
+        print(url)
+        do {
+            let data = try Data(contentsOf: url, options: [])
+            print("data:", data.count)
+            let obj = try JSONSerialization.jsonObject(with: data, options: []) as! Dictionary<String, String>
+            print("obj:", obj)
+        } catch {
+            print("Unable to load data")
+            // TODO: Stop the app
+            return
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
